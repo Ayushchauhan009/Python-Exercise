37b269ffcbef38a88320f6ec7f90730635e9ab55
Ayush chauhan <aayushchauhanrajput47@gmail.com>
2022-04-09 12:33:56 +0530

    Shorter way to write functions of a calculator

--- calc.py
+++ calc.py
@@ -3,39 +3,16 @@ class Calculator:
         self.x = 2
         self.y = 3
 
-    def add(self):
-        a = 10
-        b = 20
+    def calc(self, a, b):
         c = a + b
-        return c
-
-    def sub(self):
-        a = 10
-        b = 5
         d = a - b
-        return d
-
-    def multiply(self):
-        a = 10
-        b = 6
         y = a * b
-        return y
-
-    def division(self):
-        a = 60
-        b = 10
         z = a / b
-        return z
+        return c, d, y, z
 
 
 c1 = Calculator()
-sum = c1.add()
-diff = c1.sub()
-mul = c1.multiply()
-div = c1.division()
+cal = c1.calc(8, 2)
 
 
-print("The Sum is = ", sum)
-print("The Difference is = ", diff)
-print("The Multiplication is = ", mul)
-print("The Division is = ", div)
+print("The Calculations are = ", cal)
