08b815f23fb38ed59da0a14476fa56cf4d62235a
Ayush chauhan <aayushchauhanrajput47@gmail.com>
2022-04-09 12:14:42 +0530

    Calculator program using classes

--- calc.py
+++ calc.py
@@ -9,8 +9,33 @@ class Calculator:
         c = a + b
         return c
 
+    def sub(self):
+        a = 10
+        b = 5
+        d = a - b
+        return d
+
+    def multiply(self):
+        a = 10
+        b = 6
+        y = a * b
+        return y
+
+    def division(self):
+        a = 60
+        b = 10
+        z = a / b
+        return z
+
 
 c1 = Calculator()
 sum = c1.add()
+diff = c1.sub()
+mul = c1.multiply()
+div = c1.division()
+
 
 print("The Sum is = ", sum)
+print("The Difference is = ", diff)
+print("The Multiplication is = ", mul)
+print("The Division is = ", div)
